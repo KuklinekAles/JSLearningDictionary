87192ebf86892479e3dff566b5c259be87780257
KuklinekAles <ales@kuklinek.eu>
2025-01-02 09:34:45 +0100

    try to correct github red error, to ende html unpair element

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="cs">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/uikit.min.css">
-    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
     <script src="js/uikit.min.js"></script>
     <script src="js/uikit-icons.min.js"></script>
     <title>Učení slovíček</title>
